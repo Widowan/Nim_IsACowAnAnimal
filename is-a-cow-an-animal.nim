c3435c8f38d800eb179b867e9a229715dd6ea732
Widowan <Widowan@protonmail.com>
2019-07-11 12:36:39 +0300

    Cleaner version with enum

--- is-a-cow-an-animal.nim
+++ is-a-cow-an-animal.nim
@@ -5,42 +5,50 @@ type
     Dead = object of Exception
 
 
-    Food = ref object of RootObj
-        energy: int
+    FoodType = enum
+        Meat, Grass, Carrot
 
+    Food = ref object of RootObj
+        energy:   int
+        foodType: FoodType  
 
     Animal = ref object of RootObj
-        energy: int
-        name:   string
-        alive:  bool
-        canEat: seq[Food]
+        energy:   int
+        name:     string
+        alive:    bool
+        canEat:   seq[FoodType]
 
 
 method eat(this: Animal, food: Food): void {.base.} =
-    if not (food in this.canEat):
-        raise newException(FoodMismatch, "Mismatched food")
     if not this.alive:
         raise newException(Dead, "Animal is dead")
+    if not (food.foodtype in this.canEat):
+        raise newException(FoodMismatch, "Mismatched food")
     this.energy += food.energy
+
 
 method slaughter(this: Animal): Food {.base.} =
     if not this.alive:
         raise newException(Dead, "Animal is dead")
-    this.alive = false
-    result     = Food(energy: this.energy)
+    this.alive  = false
+    result      = Food(energy: this.energy, foodType: Meat)
+    this.energy = 0
+
+
 
 let
-    grass  = Food(energy: 5)
-    carrot = Food(energy: 10)
+    grass  = Food(energy: 5,  foodType: Grass)
+    carrot = Food(energy: 10, foodType: Carrot)
 
 var
-    rabbit = Animal(energy: 100,  name: "Rabbit", alive: true, canEat: @[carrot])
-    cow    = Animal(energy: 1000, name: "Cow",    alive: true, canEat: @[grass])
-    human  = Animal(energy: 300,  name: "Hooman", alive: true, canEat: @[carrot])
-    ahuman = Animal(energy: 350,  name: "Another Hooman", alive: true, canEat: @[carrot])
+    rabbit = Animal(energy: 100,  name: "Rabbit", alive: true, canEat: @[Carrot])
+    cow    = Animal(energy: 1000, name: "Cow",    alive: true, canEat: @[Grass])
+    human  = Animal(energy: 300,  name: "Hooman", alive: true, canEat: @[Carrot, Meat])
+    ahuman = Animal(energy: 350,  name: "Another Hooman", alive: true, canEat: @[Carrot, Meat])
 
 for i in [rabbit, cow, human, ahuman]:
     echo i.name, spaces(15-i.name.len), "-> ", i.energy
+echo ""
 
 rabbit.eat(carrot)
 cow.eat(grass)
@@ -49,7 +57,6 @@ let
     deadRabbit = rabbit.slaughter()
     beef = cow.slaughter()
 
-human.canEat.add(@[beef, deadRabbit])
 human.eat(carrot)
 human.eat(carrot)
 human.eat(beef)
@@ -57,8 +64,10 @@ human.eat(beef)
 let
     deadHuman = ahuman.slaughter()
 
-human.canEat.add(deadHuman)
 human.eat(deadHuman)
+
+for i in [rabbit, cow, human, ahuman]:
+    echo i.name, spaces(15-i.name.len), "-> ", i.energy, (if not i.alive: " [Dead]" else: "")
 
 assert(human.energy == 1675, "No, it's " & $human.energy)
 
